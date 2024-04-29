8b308c56486df3c000a75d42486420636286bbda
Marc Lainez <marc.lainez@gmail.com>
2024-04-29 23:33:27 +0200

    Rename system

--- mix.exs
+++ mix.exs
@@ -1,8 +1,8 @@
-defmodule NervesSystemRpi4.MixProject do
+defmodule NervesSystemSlutterRpi4.MixProject do
   use Mix.Project
 
   @github_organization "nerves-project"
-  @app :nerves_system_rpi4
+  @app :nerves_system_flutter_rpi4
   @source_url "https://github.com/#{@github_organization}/#{@app}"
   @version Path.join(__DIR__, "VERSION")
            |> File.read!()
