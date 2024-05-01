1efb648d50e12010c0fc4227c5f4ac018faab22a
Marc Lainez <marc.lainez@gmail.com>
2024-05-02 00:31:06 +0200

    Remove uneeded config options

--- packages/smrt-flutter-pi/smrt-flutter-pi.mk
+++ packages/smrt-flutter-pi/smrt-flutter-pi.mk
@@ -16,8 +16,6 @@ SMRT_FLUTTER_PI_DEPENDENCIES += google-libflutter libglvnd mesa3d libinput libxk
 
 SMRT_FLUTTER_PI_CONF_OPTS = \
 	-DFLUTTER_ENGINE_SHA=$(SMRT_FLUTTER_PI_ENGINE_HASH) \
-	-DLINT_EGL_HEADERS=OFF \
-	-DTRY_ENABLE_OPENGL=OFF
 
 # TODO: For some reason this is needed, investigation needed.
 define REINSTALL_LIBGLVND_LIBRARIES
@@ -31,7 +29,8 @@ ifeq ($(BR2_PACKAGE_SMRT_FLUTTER_PI_ENABLE_DEBUG),y)
 		-DCMAKE_BUILD_TYPE=Debug \
 		-DCMAKE_C_FLAGS="-O1 -Wno-use-after-free"
 else
-	SMRT_FLUTTER_PI_CONF_OPTS += -DCMAKE_C_FLAGS="-Wno-use-after-free"
+	SMRT_FLUTTER_PI_CONF_OPTS += \
+		-DCMAKE_C_FLAGS="-Wno-use-after-free"
 endif
 
 $(eval $(cmake-package))
